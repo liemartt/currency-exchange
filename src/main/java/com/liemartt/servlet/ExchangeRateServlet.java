edd7e07a7b16ab681afdd6ea2d6bc47e89b4fb9a
Artem_Limonov <liemartt@yandex.ru>
2023-12-24 00:02:30 +0300

    add url pattern for servlets

--- src/main/java/com/liemartt/servlet/ExchangeRateServlet.java
+++ src/main/java/com/liemartt/servlet/ExchangeRateServlet.java
@@ -1,12 +1,13 @@
 package com.liemartt.servlet;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-
+@WebServlet(urlPatterns = "/exchangeRate/*")
 public class ExchangeRateServlet extends HttpServlet{
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
--- src/main/java/com/liemartt/servlet/ExchangeRatesServlet.java
+++ src/main/java/com/liemartt/servlet/ExchangeRatesServlet.java
@@ -1,12 +1,13 @@
 package com.liemartt.servlet;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-
+@WebServlet(urlPatterns = "/exchangeRates")
 public class ExchangeRatesServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
