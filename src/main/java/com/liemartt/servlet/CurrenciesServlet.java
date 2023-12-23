ecd889b4073cbbedf99d0518edb13504356490fb
Artem Limonov <liemartt@yandex.ru>
2023-12-23 22:58:48 +0300

    created base structuree of servlets

---
+++ src/main/java/com/liemartt/servlet/CurrenciesServlet.java
@@ -0,0 +1,20 @@
+package com.liemartt.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class CurrenciesServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/java/com/liemartt/servlet/ExchangeRateServlet.java
@@ -0,0 +1,24 @@
+package com.liemartt.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class ExchangeRateServlet extends HttpServlet{
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    protected void doPatch(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
