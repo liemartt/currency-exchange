1449c0e59a8ec1baf985a6de0da4d37ae82e7b08
Artem_Limonov <liemartt@yandex.ru>
2024-02-24 20:23:40 +0300

    added message class for errors

--- src/main/java/com/liemartt/servlet/ErrorSender.java
+++ src/main/java/com/liemartt/servlet/ErrorSender.java
@@ -1,14 +1,16 @@
 package com.liemartt.servlet;
 
 import com.google.gson.Gson;
+import com.liemartt.model.Message;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
 public class ErrorSender {
     public static void send(HttpServletResponse resp, int code, String message) throws IOException {
+        String jsonMessage = new Gson().toJson(new Message(message));
         resp.setContentType("application/json");
-        String jsonMessage = new Gson().toJson(message);
-        resp.sendError(code, jsonMessage);
+        resp.setStatus(code);
+        resp.getWriter().write(jsonMessage);
     }
 }
