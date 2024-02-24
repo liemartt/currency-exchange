57ecb66f8bce66de8389dfe42c772ff2e1b0938f
Artem_Limonov <liemartt@yandex.ru>
2024-02-24 18:20:07 +0300

    implemented currency servlet

--- src/main/java/com/liemartt/servlet/CurrencyServlet.java
+++ src/main/java/com/liemartt/servlet/CurrencyServlet.java
@@ -1,5 +1,9 @@
 package com.liemartt.servlet;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.liemartt.dao.CurrencyDAOImpl;
+import com.liemartt.model.Currency;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,11 +11,25 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet(urlPatterns = "/currency/*")
 public class CurrencyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String pathInfo = req.getPathInfo();
+        if (!pathInfo.matches("^/[A-Z]{3}$")) {
+            ErrorSender.send(resp, 400, "path must be /currency/***");
+            return;
+        }
+        String currencyCode = pathInfo.split("/")[1];
+        try {
+            Currency currency = new CurrencyDAOImpl().getCurrencyByCode(currencyCode);
+            if (currency != null) Renderer.render(resp, currency);
+            else ErrorSender.send(resp, 404, "No such currency");
+        } catch (SQLException e) {
+            ErrorSender.send(resp, 500, "");
+        }
 
     }
 }
