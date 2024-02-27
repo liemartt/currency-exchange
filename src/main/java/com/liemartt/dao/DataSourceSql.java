6cf372696b33e8027fb0314d069c65d20f22a7cf
Artem_Limonov <liemartt@yandex.ru>
2024-02-27 18:11:58 +0300

    fix: changed absolute path to the Class loader realization

--- src/main/java/com/liemartt/dao/DataSourceSql.java
+++ src/main/java/com/liemartt/dao/DataSourceSql.java
@@ -10,7 +10,7 @@ public class DataSourceSql {
     public final static SQLiteDataSource dataSource;
 
     static {
-        String URL = "jdbc:sqlite:D:/JavaLearning/currency-exchange/src/main/resources/currency-exchange-db.sqlite";
+        String URL = "jdbc:sqlite:"+DataSourceSql.class.getClassLoader().getResource("currency-exchange-db.sqlite").getPath();
         dataSource = new SQLiteDataSource();
         dataSource.setUrl(URL);
     }
